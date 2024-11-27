931165dbf2030b5265a9e68a4cb586039d9f199f
mattuu0 <tempacountyt@gmail.com>
2024-11-28 00:38:56 +0900

    チーム作成、リスト、削除 完成

--- nginx/statics/dev/scripts/team.js
+++ nginx/statics/dev/scripts/team.js
@@ -15,6 +15,9 @@ create_team.addEventListener("submit", async (evt) => {
 
         console.log(team);
 
+        // チームリスト更新
+        await RefreshTeam();
+
     } catch (ex) {
         console.error(ex);
     }
@@ -55,51 +58,84 @@ async function ListTeam() {
     return req.json();
 }
 
-// 初期化関数
-async function Init() {
+
+// チームリスと取得
+const teamlist = document.getElementById("team_list");
+
+// チームを表示する関数
+function ShowTeam(id, name, creator, status) {
+    const basetr = document.createElement("tr");
+
+    // チームID
+    const idtd = document.createElement("td");
+    idtd.textContent = id;
+
+    // チーム名
+    const nametd = document.createElement("td");
+    nametd.textContent = name;
+
+    // 作成者   
+    const creatortd = document.createElement("td");
+    creatortd.textContent = creator;
+
+    // 状態
+    const statustd = document.createElement("td");
+    statustd.textContent = status;
+
+    // 削除ボタン
+    const deltd = document.createElement("td");
+    const delbtn = document.createElement("button");
+    delbtn.textContent = "削除";
+    delbtn.addEventListener("click",async () => {
+        console.log(await DeleteTeam(id));
+
+        // リロード
+        RefreshTeam();
+    });
+    deltd.appendChild(delbtn);
+
+    // 追加
+    basetr.appendChild(idtd);
+    basetr.appendChild(nametd);
+    basetr.appendChild(creatortd);
+    basetr.appendChild(statustd);
+    basetr.appendChild(deltd);
+
+    teamlist.appendChild(basetr);
+}
+
+async function DeleteTeam(teamid) {
+    // トークンを取得
+    const token = await GetToken();
+
+    // リクエストを送る
+    const req = await fetch("/admin/team/delete", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token,
+        },
+        body: JSON.stringify({ "teamid": teamid }),
+    });
+
+    return req.json();
+}
+
+async function RefreshTeam() {
+    // チームリストをクリア
+    teamlist.innerHTML = "";
+
     const teams = await ListTeam();
     console.log(teams["msg"]);
 
-    // チームリスと取得
-    const teamlist = document.getElementById("team_list");
-
     teams["msg"].forEach((team) => {
-        const basetr = document.createElement("tr");
-
-        // チームID
-        const idtd = document.createElement("td");
-        idtd.textContent = team["TeamID"];
-
-        // チーム名
-        const nametd = document.createElement("td");
-        nametd.textContent = team["Name"];
-
-        // 作成者   
-        const creatortd = document.createElement("td");
-        creatortd.textContent = team["Creator"];
-
-        // 状態
-        const statustd = document.createElement("td");
-        statustd.textContent = team["Status"];
-
-        // 削除ボタン
-        const deltd = document.createElement("td");
-        const delbtn = document.createElement("button");
-        delbtn.textContent = "削除";
-        delbtn.addEventListener("click", () => {
-            DeleteTeam(team["TeamID"]);
-        });
-        deltd.appendChild(delbtn);
-
-        // 追加
-        basetr.appendChild(idtd);
-        basetr.appendChild(nametd);
-        basetr.appendChild(creatortd);
-        basetr.appendChild(statustd);
-        basetr.appendChild(deltd);
-        
-        teamlist.appendChild(basetr);
+        ShowTeam(team["TeamID"], team["Name"], team["Creator"], team["Status"]);
     });
+}
+
+// 初期化関数
+async function Init() {
+    await RefreshTeam();
 }
 
 Init();
